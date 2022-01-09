748d6dedb8dfae167a40fbad5f15caf6749c0113
Guillermo Bandres <guillermo.bandres@paack.co>
2022-01-09 20:14:23 +0100

    wip

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-$(function() {
+// gsap.registerPlugin(ScrollTrigger);
 
-    var $body = $(document);
-    $body.bind('scroll', function() {
-        // "Disable" the horizontal scroll.
-        if ($body.scrollLeft() !== 0) {
-            $body.scrollLeft(0);
-        }
-    });
+// gsap.defaults({
+//     ease: "none",
+//     duration: 1
+// })
 
-}); 
+// const animation1 = gsap.timeline();
+// // animation1.from(".p2-img", { y: 50})
+// animation1.from(".p2-text", { opacity: 0, y: 100})
+// animation1.from(".p3", {width: "0%"})
+
+// ScrollTrigger.create({
+//     animation: animation1,
+//     trigger: "#animation-1",
+//     start: "top top",
+//     scrub: true,
+//     pin: true,
+//     markers: true
+// })
